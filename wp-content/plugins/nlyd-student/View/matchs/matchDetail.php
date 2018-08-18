8cb98fe7b3a1645417a8b4085eb9dd1fe44e47e5
weihaidong <296656132@qq.com>
2018-08-18 11:34:08 +0800

    修改比赛详情

--- wp-content/plugins/nlyd-student/View/matchs/matchDetail.php
+++ wp-content/plugins/nlyd-student/View/matchs/matchDetail.php
@@ -101,6 +101,9 @@
                         <?php if( $match['is_me'] == 'y' && $match['match_status'] == 2):?>
                             <a class="a-btn" href="<?=home_url('/matchs/matching/match_id/'.$_GET['match_id']);?>">进入比赛</a>
                         <?php endif;?>
+                        <?php if($match['match_status'] == -3):?>
+                            <a class="a-btn" href="<?=home_url('/matchs/record/match_id/'.$_GET['match_id']);?>">查看战绩</a>
+                        <?php endif;?>
                     </div>
                 </div>
             </div>
