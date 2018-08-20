a9602b5f2488cae63968dc86e6494ab45ee3f0e5
weihaidong <296656132@qq.com>
2018-08-20 17:08:34 +0800

    ajax获取新闻列表

--- wp-content/plugins/nlyd-student/View/news/news-list.php
+++ wp-content/plugins/nlyd-student/View/news/news-list.php
@@ -9,8 +9,8 @@
                 <h1 class="mui-title">行业新闻</h1>
             </header>
             <div class="layui-row nl-border nl-content">
-                <div class="width-margin width-margin-pc news-wrapper lauui-row">
-                    <!-- <?php foreach ($rows as $row){ ?>
+                <div class="width-margin width-margin-pc news-wrapper layui-row flow-default">
+                    <?php foreach ($rows as $row){ ?>
                         <a class="nl-ad-row layui-bg-white" href="<?=home_url('/student/account/news/?action=newsDetail&id='.$row->ID);?>">
                             <div class="layui-row foot-info">
                                 <div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">
@@ -22,41 +22,41 @@
                                 </div>
                             </div>
                         </a>
-                    <?php } ?> -->
+                    <?php } ?>
 
-                   <a class="nl-ad-row layui-bg-white" href="<?//=home_url('/student/account/news/?action=newsDetail');?><!--">-->
-                       <div class="layui-row foot-info">
-                           <div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">
-                               <img src="<?//=student_css_url.'image/homePage/course1.png'?><!--">-->
-                           </div>
-                           <div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">
-                               <p class="nl-ad-name">脑力运动测评体系让脑力运脑力运动测评体系让脑力运</p>
-                               <p class="nl-ad-detail">近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测</p>
-                           </div>
-                       </div>
-                   </a>
-                   <a class="nl-ad-row layui-bg-white" href="<?//=home_url('/student/account/news/?action=newsDetail');?><!--">-->
-                       <div class="layui-row foot-info">
-                           <div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">
-                               <img src="<?//=student_css_url.'image/homePage/course1.png'?><!--">-->
-                           </div>
-                           <div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">
-                               <p class="nl-ad-name">脑力运动测评体系让脑力运脑力运动测评体系让脑力运</p>
-                               <p class="nl-ad-detail">近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测</p>
-                           </div>
-                       </div>
-                   </a>
-                   <a class="nl-ad-row layui-bg-white" href="<?//=home_url('/student/account/news/?action=newsDetail');?><!--">-->
-                       <div class="layui-row foot-info">
-                           <div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">
-                               <img src="<?//=student_css_url.'image/homePage/course1.png'?><!--">-->
-                           </div>
-                           <div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">
-                               <p class="nl-ad-name">脑力运动测评体系让脑力运脑力运动测评体系让脑力运</p>
-                               <p class="nl-ad-detail">近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测</p>
-                           </div>
-                       </div>
-                   </a>
+<!--                    <a class="nl-ad-row layui-bg-white" href="--><?//=home_url('/student/account/news/?action=newsDetail');?><!--">-->
+<!--                        <div class="layui-row foot-info">-->
+<!--                            <div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">-->
+<!--                                <img src="--><?//=student_css_url.'image/homePage/course1.png'?><!--">-->
+<!--                            </div>-->
+<!--                            <div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">-->
+<!--                                <p class="nl-ad-name">脑力运动测评体系让脑力运脑力运动测评体系让脑力运</p>-->
+<!--                                <p class="nl-ad-detail">近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </a>-->
+<!--                    <a class="nl-ad-row layui-bg-white" href="--><?//=home_url('/student/account/news/?action=newsDetail');?><!--">-->
+<!--                        <div class="layui-row foot-info">-->
+<!--                            <div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">-->
+<!--                                <img src="--><?//=student_css_url.'image/homePage/course1.png'?><!--">-->
+<!--                            </div>-->
+<!--                            <div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">-->
+<!--                                <p class="nl-ad-name">脑力运动测评体系让脑力运脑力运动测评体系让脑力运</p>-->
+<!--                                <p class="nl-ad-detail">近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </a>-->
+<!--                    <a class="nl-ad-row layui-bg-white" href="--><?//=home_url('/student/account/news/?action=newsDetail');?><!--">-->
+<!--                        <div class="layui-row foot-info">-->
+<!--                            <div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">-->
+<!--                                <img src="--><?//=student_css_url.'image/homePage/course1.png'?><!--">-->
+<!--                            </div>-->
+<!--                            <div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">-->
+<!--                                <p class="nl-ad-name">脑力运动测评体系让脑力运脑力运动测评体系让脑力运</p>-->
+<!--                                <p class="nl-ad-detail">近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </a>-->
                 </div>
             </div>
         </div>
--- wp-content/themes/dt-armada-child/archive.php
+++ wp-content/themes/dt-armada-child/archive.php
@@ -30,54 +30,8 @@ get_header(); ?>
 								<h1 class="mui-title">行业新闻</h1>
 							</header>
 							<div class="layui-row nl-border nl-content">
-								<div class="width-margin width-margin-pc news-wrapper lauui-row">
-									<!-- <?php foreach ($rows as $row){ ?>
-										<a class="nl-ad-row layui-bg-white" href="<?=home_url('/student/account/news/?action=newsDetail&id='.$row->ID);?>">
-											<div class="layui-row foot-info">
-												<div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">
-													<img src="<?=wp_get_attachment_image_src(get_post_thumbnail_id($row->ID), 'thumbnail')[0]?>">
-												</div>
-												<div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">
-													<p class="nl-ad-name"><?=$row->post_title?></p>
-													<p class="nl-ad-detail"><?=strip_tags($row->post_content)?></p>
-												</div>
-											</div>
-										</a>
-									<?php } ?> -->
+								<div class="width-margin width-margin-pc news-wrapper layui-row flow-default" id="flow">
 
-								<a class="nl-ad-row layui-bg-white" href="<?=home_url('/你好资讯/');?>">
-									<div class="layui-row foot-info">
-										<div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">
-											<img src="<?=student_css_url.'image/homePage/course1.png'?>">
-										</div>
-										<div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">
-											<p class="nl-ad-name">脑力运动测评体系让脑力运脑力运动测评体系让脑力运</p>
-											<p class="nl-ad-detail">近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测</p>
-										</div>
-									</div>
-								</a>
-								<a class="nl-ad-row layui-bg-white" href="<?=home_url('/你好资讯/');?>">
-									<div class="layui-row foot-info">
-										<div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">
-											<img src="<?=student_css_url.'image/homePage/course1.png'?>">
-										</div>
-										<div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">
-											<p class="nl-ad-name">脑力运动测评体系让脑力运脑力运动测评体系让脑力运</p>
-											<p class="nl-ad-detail">近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测</p>
-										</div>
-									</div>
-								</a>
-								<a class="nl-ad-row layui-bg-white" href="<?=home_url('/你好资讯/');?>">
-									<div class="layui-row foot-info">
-										<div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">
-											<img src="<?=student_css_url.'image/homePage/course1.png'?>">
-										</div>
-										<div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">
-											<p class="nl-ad-name">脑力运动测评体系让脑力运脑力运动测评体系让脑力运</p>
-											<p class="nl-ad-detail">近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测近日，由脑力中国授权并经国际脑力运动委员会（IISC）备案的全国几十家脑力运动专业机构纷纷开展“国际脑力水平等级测</p>
-										</div>
-									</div>
-								</a>
 								</div>
 							</div>
 						</div>
@@ -89,3 +43,59 @@ get_header(); ?>
 			<?php do_action('presscore_after_content'); ?>
 
 <?php get_footer(); ?>
+<script>
+jQuery(function($) {   
+    layui.use(['layer','flow'], function(){
+        var flow = layui.flow;//流加载
+//--------------------分页--------------------------
+	flow.load({
+            elem: '#flow'
+            ,scrollElem: '#flow'
+            ,isAuto: false
+            ,isLazyimg: true
+            ,done: function(page, next){ //加载下一页
+                var postData={
+                    action:'getNewsLists',
+                    page:page,
+                }
+                var lis = [];
+                $.post(window.admin_ajax+"?date="+new Date().getTime(),postData,function(res,ajaxStatu,xhr){
+					console.log(res)
+					if(res.success){
+						$.each(res.data.info,function(i,v){
+							
+							var dom='<a class="nl-ad-row layui-bg-white" href="'+v.guid+'">'
+										+'<div class="layui-row foot-info">'
+											+'<div class="layui-col-lg5 layui-col-md5 layui-col-sm5 layui-col-xs5 img-box">'
+												+'<img src="">'
+											+'</div>'
+											+'<div class="layui-col-lg7 layui-col-md7 layui-col-sm7 layui-col-xs7">'
+												+'<p class="nl-ad-name">'+v.post_title+'</p>'
+												+'<p class="nl-ad-detail">'+v.post_name+'</p>'
+											+'</div>'
+										+'</div>'
+									+'</a>'
+							lis.push(dom) 
+						})
+						if (res.data.info.length<10) {
+							next(lis.join(''),false) 
+						}else{
+							next(lis.join(''),true) 
+						}
+						
+					}else{
+						if(page==1){
+							var dom='<div class="no-info">无新闻信息</div>'
+							lis.push(dom) 
+						}else{
+							$.alerts('没有更多了')
+						}
+						next(lis.join(''),false)
+					}
+                })       
+            }
+        });
+ //--------------------分页--------------------------  
+    })
+})
+</script>
