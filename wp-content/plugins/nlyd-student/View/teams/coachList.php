0b479f0fd83d486611da9feb5d691cae9450ae32
weihaidong <296656132@qq.com>
2018-08-22 20:19:45 +0800

    xiugai

--- wp-content/plugins/nlyd-student/View/teams/coachList.php
+++ wp-content/plugins/nlyd-student/View/teams/coachList.php
@@ -96,13 +96,14 @@ layui.use(['element','flow','layer'], function(){
         var _this=$(this);
         var coach_id=_this.attr('data-coachId');
         var category_id=_this.attr('data-categoryId');
+        var coach_name=_this.attr('data-coachName')
             layer.open({
             type: 1
             ,maxWidth:300
-            ,title: '提示' //不显示标题栏
+            ,title: '教练申请' //不显示标题栏
             ,skin:'nl-box-skin'
             ,id: 'certification' //防止重复弹出
-            ,content: '<div class="box-conent-wrapper">是否请TA当教练？</div>'
+            ,content: '<div class="box-conent-wrapper">是否确认向“'+coach_name+'”发送教练申请？</div>'
             ,btn: ['再想想', '确认', ]
             ,success: function(layero, index){
                 
@@ -120,7 +121,9 @@ layui.use(['element','flow','layer'], function(){
                 $.post(window.admin_ajax+"?date="+new Date().getTime(),postData,function(res){
                     $.alerts(res.data.info)
                     if(res.success){
-                        window.location.reload()
+                        setTimeout(() => {
+                            window.location.reload()
+                        }, 1600);
                     }
                     
                 })
@@ -136,13 +139,15 @@ layui.use(['element','flow','layer'], function(){
         var _this=$(this);
         var coach_id=_this.attr('data-coachId');
         var category_id=_this.attr('data-categoryId');
+        var coach_name=_this.attr('data-coachName')
+        var type=$('.layui-this a').text()
             layer.open({
             type: 1
             ,maxWidth:300
-            ,title: '提示' //不显示标题栏
+            ,title: '设置主训教练' //不显示标题栏
             ,skin:'nl-box-skin'
             ,id: 'certification' //防止重复弹出
-            ,content: '<div class="box-conent-wrapper">是否立即更换主训教练？</div>'
+            ,content: '<div class="box-conent-wrapper">是否确认设置“'+coach_name+'”为'+type+'主训教练？</div>'
             ,btn: ['再想想', '确认', ]
             ,success: function(layero, index){
                 
@@ -164,6 +169,62 @@ layui.use(['element','flow','layer'], function(){
                     category_id:category_id,
                 }
                 $.post(window.admin_ajax+"?date="+new Date().getTime(),postData,function(res){
+                    if(res.success){
+                        if(res.data.url.length>0){
+                            window.location.href=res.data.url
+                        }else{
+                            window.location.reload()
+                        }
+
+                    }else{
+                        $.alerts(res.data.info)
+                    }
+
+                })
+            }
+            ,closeBtn:2
+            ,btnAagn: 'c' //按钮居中
+            ,shade: 0.3 //遮罩
+            ,isOutAnim:true//关闭动画
+        });
+        return false
+    })
+
+    $('body').on('click','.clearMain',function(){//解除主训
+        var _this=$(this);
+        var coach_id=_this.attr('data-coachId');
+        var category_id=_this.attr('data-categoryId');
+        var coach_name=_this.attr('data-coachName')
+        var type=$('.layui-this a').text()
+            layer.open({
+            type: 1
+            ,maxWidth:300
+            ,title: '解除主训教学关系' //不显示标题栏
+            ,skin:'nl-box-skin'
+            ,id: 'certification' //防止重复弹出
+            ,content: '<div class="box-conent-wrapper">您是否确认解除与“'+coach_name+'”的主训关系？</div>'
+            ,btn: ['再想想', '确认', ]
+            ,success: function(layero, index){
+                
+            }
+            ,yes: function(index, layero){
+                layer.closeAll();
+            }
+            ,btn2: function(index, layero){
+                var match_id = <?=!empty($_GET['match_id']) ? $_GET['match_id'] : "''"?>;
+                /*var match_id=''
+                if($.Request('match_id')!=null){
+                    match_id=$.Request('match_id')
+                }*/
+                var postData={
+                    action:'set_major_coach',
+                    _wpnonce:$('#setMain').val(),
+                    coach_id:coach_id,
+                    // match_id:match_id,
+                    category_id:category_id,
+                }
+                $.post(window.admin_ajax+"?date="+new Date().getTime(),postData,function(res){
+                    console.log(res)
                     if(res.success){
                         if(res.data.url.length>0){
                             window.location.href=res.data.url
@@ -234,29 +295,39 @@ layui.use(['element','flow','layer'], function(){
                                 }
                                 if(v.apply_status!=null){//1，申请中，2我的教练
                                     if(v.apply_status==1){//1，申请中，2我的教练
-                                        coach_btn='<div class="right_c"><div class="coach-btn text_1 ">教练审核中...</div></div>';
+                                        coach_btn='<div class="right_c"><div class="coach-btn bg_gradient_blue text_1 ">教练审核中...</div></div>';
                                         isLeft="ta_l"
                                     }else if(v.apply_status==2){//1，申请中，2我的教练
                                         
                                         if(v.my_major_coach=='y'){//主训教练
-                                            major_coach='<div class="nl-badge"><i class="iconfont">&#xe608;</i></div>';
-                                            clear_btn='<span class="clear fl_1 text_1 ta_l" data-coachId="'+v.coach_id+'" data-categoryId="'+v.category_id+'">解除主训关系</span>'
+                                            
+                                            major_coach='<div class="nl-badge bg_gradient_orange"><i class="iconfont">&#xe608;</i></div>';
+                                            clear_btn='<span class="clearMain fl_1 text_1 ta_l"  data-coachName="'+v.display_name+'" data-coachId="'+v.coach_id+'" data-categoryId="'+v.category_id+'">解除主训关系</span>'
                                         }else{
-                                            major_coach='<div class="nl-badge"><i class="iconfont">&#xe608;</i></div>';
-                                            coach_btn='<div class="right_c"><div class="coach-btn text_1  bg_gradient_orange setCoach" data-coachId="'+v.coach_id+'" data-categoryId="'+v.category_id+'">设为主训教练</div></div>';
-                                            clear_btn='<span class="clear fl_1 text_1 ta_l" data-coachId="'+v.coach_id+'" data-categoryId="'+v.category_id+'">解除教学关系</span>'
+                                            major_coach='<div class="nl-badge bg_gradient_blue"><i class="iconfont">&#xe608;</i></div>';
+                                            coach_btn='<div class="right_c"><div class="coach-btn text_1  bg_gradient_orange setCoach" data-coachName="'+v.display_name+'" data-coachId="'+v.coach_id+'" data-categoryId="'+v.category_id+'">设为主训教练</div></div>';
+                                            clear_btn='<span class="clearCoach fl_1 text_1 ta_l" data-coachName="'+v.display_name+'"  data-coachId="'+v.coach_id+'" data-categoryId="'+v.category_id+'">解除教学关系</span>'
                                         }
                                         $.each(arr,function(index,value){//橘色高亮
                                             if(value.ID==$('.layui-this').attr('data-id')){
+
                                                 if(value.post_title=='速算类'){
-                                                    computeClass='c_orange';
                                                     compute_major_coach=major_coach;
+                                                    if(v.my_major_coach=='y'){
+                                                        computeClass='c_orange';
+                                                    }
                                                 }else if(value.post_title=='速记类'){
-                                                    memoryClass='c_orange';
+                                                    // memoryClass='c_orange';
                                                     memory_major_coach=major_coach;
+                                                    if(v.my_major_coach=='y'){
+                                                        memoryClass='c_orange';
+                                                    }
                                                 }else if(value.post_title=='速读类'){
-                                                    readClass='c_orange';
+                                                    // readClass='c_orange';
                                                     read_major_coach=major_coach;
+                                                    if(v.my_major_coach=='y'){
+                                                        readClass='c_orange';
+                                                    }
                                                 }
                                             }
                                         })
@@ -270,7 +341,7 @@ layui.use(['element','flow','layer'], function(){
                                     isMyCoach='<div class="coach-type text_1 '+readClass+'">速读类</div>'
                                               +'<div class="coach-type text_1 '+memoryClass+'">记忆类</div>'
                                               +'<div class="coach-type text_1 '+computeClass+'">速算类</div>'
-                                    coach_btn='<div class="right_c"><div class="coach-btn text_1 setTeacher" data-coachId="'+v.coach_id+'" data-categoryId="'+v.category_id+'">请TA当教练</div></div>';//不是我的教练
+                                    coach_btn='<div class="right_c"><div class="coach-btn bg_gradient_blue text_1 setTeacher" data-coachName="'+v.display_name+'" data-coachId="'+v.coach_id+'" data-categoryId="'+v.category_id+'">请TA当教练</div></div>';//不是我的教练
                                     isLeft="ta_l"
                                 }
                                 var dom='<li class="layui-col-lg4 layui-col-md4 layui-col-sm12 layui-col-xs12">'
