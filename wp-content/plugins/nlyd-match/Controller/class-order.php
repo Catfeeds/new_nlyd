fd8281bf3c870fe779036dc9fe84724f92097a6f
luoxiao159 <1115996550@qq.com>
2018-08-23 09:38:10 +0800

    收货地址返回路径,订单非未支付和已退款和失效订单都可以退款

--- wp-content/plugins/nlyd-match/Controller/class-order.php
+++ wp-content/plugins/nlyd-match/Controller/class-order.php
@@ -191,8 +191,7 @@ class Order {
                                         <?php
                                             switch ($row['pay_status']){
                                                 case -1:
-                                                    echo '<span class="edit"><a href="javascript:;" class="no_refund">不退款</a> | </span>';
-                                                    echo '<span class="delete"><a class="submitdelete" href="?page=order-refund&serial=<?=$row[\'serialnumber\']?>">退款</a>  </span>';
+//                                                    echo '<span class="edit"><a href="javascript:;" class="no_refund">不退款</a> | </span>';
                                                     break;
                                                 case 2:
                                                         echo '<span class="edit"><a href="?page=order-send&id='.$row['id'].'" class="deliver">发货</a> </span>';
@@ -203,6 +202,10 @@ class Order {
                                                     }
                                                     break;
                                             }
+                                        if($row['pay_status'] != -2 && $row['pay_status'] != 1 && $row['pay_status'] != 5){
+                                            //不是已退款,不是未支付,不是失效订单
+                                            echo '<span class="delete"><a class="submitdelete" href="?page=order-refund&serial='.$row['serialnumber'].'">退款</a>  </span>';
+                                        }
                                         ?>
                                     </div>
                                     <button type="button" class="toggle-row"><span class="screen-reader-text">显示详情</span></button>
