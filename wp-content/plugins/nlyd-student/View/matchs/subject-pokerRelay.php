85a7b98db08b0e177c8fe6f658bcca38d8a52ad1
weihaidong <296656132@qq.com>
2018-09-21 19:08:24 +0800

    修改

--- wp-content/plugins/nlyd-student/View/matchs/subject-pokerRelay.php
+++ wp-content/plugins/nlyd-student/View/matchs/subject-pokerRelay.php
@@ -79,46 +79,5 @@
                             </div>
                         </div>
                         <div class="answerBtn porkerAnswer">正确答案</div>
-                        <!-- <div class="right-answer">
-                            <div class="porker-zoo">
-                                <div class="poker-window">
-                                    <?php if(!empty($questions_answer)): ?>
-                                    <div class="poker-wrapper">
-                                        <?php foreach ($questions_answer as $k => $v ){
-                                                $val = str2arr($v,'-');
-                                                switch ($val[0]){
-                                                    case 'club':
-                                                        $ico = '&#xe635;';
-                                                        break;
-                                                    case 'heart':
-                                                        $ico = '&#xe638;';
-                                                        break;
-                                                    case 'spade':
-                                                        $ico = '&#xe636;';
-                                                        break;
-                                                    case 'diamond':
-                                                        $ico = '&#xe634;';
-                                                        break;
-                                                }
-                                        ?>
-                                            <div class="poker <?=$val[0]?> <?= in_array($k,$error_arr) ? 'active' : '';?>">
-                                                <div class="poker-detail poker-top">
-                                                    <div class="poker-name"><?=$val[1]?></div>
-                                                    <div class="poker-type"><i class="iconfont"><?=$ico?></i></div>
-                                                </div>
-                                                <div class="poker-logo">
-                                                    <img src="<?=student_css_url.'image/nlyd-big.png'?>">
-                                                </div>
-                                                <div class="poker-detail poker-bottom">
-                                                    <div class="poker-name"><?=$val[1]?></div>
-                                                    <div class="poker-type"><i class="iconfont"><?=$ico?></i></div>
-                                                </div>
-                                            </div>
-                                        <?php } ?>
-                                    </div>
-                                    <?php endif;?>
-                                </div>
-                            </div>
-                        </div> -->
                     </div>
                
