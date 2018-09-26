0221a91c14c0bc0711485d4e9fae7d75232ad75e
weihaidong <296656132@qq.com>
2018-09-26 13:37:13 +0800

    签到

--- wp-content/plugins/nlyd-student/View/signs/success.php
+++ wp-content/plugins/nlyd-student/View/signs/success.php
@@ -9,32 +9,34 @@
 </style>
 <div class="layui-fluid">
     <div class="layui-row">
-        <?php
-                require_once leo_student_public_view.'leftMenu.php';
-            
-        ?>
-
-
         <div class="nl-right-content layui-col-sm12 layui-col-xs12 layui-col-md12 detail-content-wrapper">   
             <div class="layui-row nl-border nl-content">
                 <div class="signs_box">
-                    <div class="img-box">
+                    <div class="img-box right_img">
                         <img src="<?=student_css_url.'image/signs/success.png'?>">
                     </div> 
-                    <h1 class="signs_tips">恭喜你，签到成功</h1>
-                    <p class="signs_title">“达智优”2018脑力世界杯中国赛</p>
-                    <div class="signs_row">
-                        <span class="signs_label">选手姓名：</span>
+                    <h3 class="signs_tips">恭喜你，签到成功</h3>
+                    <p class="signs_title fs_16 c_black">“达智优”2018脑力世界杯中国赛</p>
+                    <div class="signs_row fs_16">
+                        <span class="signs_label c_black">选手姓名：</span>
                         <span class="signs_value">孙中则</span>
                     </div>
-                    <div class="signs_row">
-                        <span class="signs_label">证件号码：</span>
+                    <div class="signs_row fs_16">
+                        <span class="signs_label c_black">证件号码：</span>
                         <span class="signs_value">5111**********5144</span>
                     </div>
-                    <div class="signs_row">
-                        <span class="signs_label">城市信息：</span>
+                    <div class="signs_row fs_16">
+                        <span class="signs_label c_black">城市信息：</span>
                         <span class="signs_value">内蒙古巴彦卓尔</span>
                     </div>
+                    <a href="<?=home_url('/matchs');?>" class="go_match"><span>去赛事中心</span></a>
+                </div>
+
+                <div class="signs_footer">
+                    <div class="img-box logo_img">
+                        <img src="<?=student_css_url.'image/nlyd-big.png'?>">
+                    </div> 
+                    <p class="ta_c fs_16">2018脑力世界杯赛事组委会</p>
                 </div>
             </div>
         </div>           
