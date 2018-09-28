0bc13622d742aa818345f5fe4d37540599d9be1a
weihaidong <296656132@qq.com>
2018-09-28 16:08:10 +0800

    9.28 答题记录未作答修改

--- wp-content/plugins/nlyd-student/View/matchs/subject-fastScan.php
+++ wp-content/plugins/nlyd-student/View/matchs/subject-fastScan.php
@@ -6,7 +6,7 @@
                                 <?php foreach ($questions_answer as $k => $val){ ?>
                                 <div class="one-ques">
                                     <p class="question"><?=$k+1?>、<?=$val?></p>
-                                    <p class="yours">你的答案：<span class="<?=$my_answer[$k] == $val ? 'yes' : 'error';?>"><?=$my_answer[$k]?></span></p>
+                                    <p class="yours">你的答案：<span class="<?=$my_answer[$k] == $val ? 'yes' : 'error';?>"><?= !empty($my_answer[$k]) ? $my_answer[$k] : '未作答' ;?></span></p>
                                     <p class="rights">正确答案：<?=$val?></p>
                                 </div>
                                 <?php } ?>
