859819ea774b0b2708f94db5cbd182248735ca57
luoxiao159 <1115996550@qq.com>
2018-10-15 20:53:24 +0800

    10-15 8 第%轮修改

--- wp-content/plugins/nlyd-student/View/matchs/matching-reading.php
+++ wp-content/plugins/nlyd-student/View/matchs/matching-reading.php
@@ -9,7 +9,7 @@
                 <form class="layui-form" lay-filter='reading'>
                     <div class="remember width-margin width-margin-pc">
                         <div class="matching-row">
-                            <span class="c_black match_info_font"><?=$project_title?><span class="blue-font"><?=printf(__('第%轮', 'nlyd-student'), $match_more_cn)?></span></span>
+                            <span class="c_black match_info_font"><?=$project_title?><span class="blue-font"><?=printf(__('第%s轮', 'nlyd-student'), $match_more_cn)?></span></span>
                             <span class="c_blue ml_10 match_info_font"><?=__('第', 'nlyd-student')?><span id="number">1</span>/<?=!empty($match_questions) ? count($match_questions) : 1?><?=__('题', 'nlyd-student')?></span>
                             <span class="c_blue ml_10 match_info_font">
                                 <i class="iconfont">&#xe685;</i>
