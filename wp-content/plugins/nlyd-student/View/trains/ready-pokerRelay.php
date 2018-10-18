5e92569ddd7031252add1d04ecdfa7fea30ad7a7
weihaidong <296656132@qq.com>
2018-10-18 21:01:21 +0800

    xiugai

--- wp-content/plugins/nlyd-student/View/trains/ready-pokerRelay.php
+++ wp-content/plugins/nlyd-student/View/trains/ready-pokerRelay.php
@@ -51,20 +51,19 @@
 jQuery(function($) { 
     var data_match=[];
     var questions_answer=[]
-    var new_poker=[]
-    var arrColor=['heart','club','diamond','spade']
-    var arrNum=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-    var arrZM=['A','J','Q','K'];
+    var new_poker=["heart-A","heart-2","heart-3","heart-4","heart-5","heart-6","heart-7","heart-8","heart-9","heart-10","heart-J","heart-Q","heart-K","club-A","club-2","club-3","club-4","club-5","club-6","club-7","club-8","club-9","club-10","club-J","club-Q","club-K","diamond-A","diamond-2","diamond-3","diamond-4","diamond-5","diamond-6","diamond-7","diamond-8","diamond-9","diamond-10","diamond-J","diamond-Q","diamond-K","spade-A","spade-2","spade-3","spade-4","spade-5","spade-6","spade-7","spade-8","spade-9","spade-10","spade-J","spade-Q","spade-K"];   
+    // var arrColor=['heart','club','diamond','spade']
+    // var arrNum=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
     var ready_poker= $.GetCookie('train_match','1');
     if(ready_poker && ready_poker['genre_id']==$.Request('genre_id') && ready_poker['type']=='pkjl'){
         questions_answer=ready_poker['train_questions']
     }else{
-        $.each(arrColor,function(i,v){
-            $.each(arrNum,function(index,val){
-                var item=v+'-'+val;
-                new_poker.push(item)
-            })
-        })
+        // $.each(arrColor,function(i,v){
+        //     $.each(arrNum,function(index,val){
+        //         var item=v+'-'+val;
+        //         new_poker.push(item)
+        //     })
+        // })
         function splits(str) {
             return str.split('-');
         }
