27da7b2da0246854efdfe2b7960743be9c8d3f85
weihaidong <296656132@qq.com>
2018-10-24 11:02:25 +0800

    xiugai

--- wp-content/plugins/nlyd-student/View/matchs/record.php
+++ wp-content/plugins/nlyd-student/View/matchs/record.php
@@ -129,9 +129,9 @@
                                 <div class="btn-wrap">
                                     <div class="classify-btn" data-post-id="1"><?=__('战队排名', 'nlyd-student')?></div>
                                 </div>
-                                <!-- <div class="btn-wrap">
+                                <div class="btn-wrap">
                                     <div class="classify-btn" data-post-id="2"><?=__('奖金明细', 'nlyd-student')?></div>
-                                </div> -->
+                                </div>
                             </div>
                             <div class="nl-table-wapper">
                                 <table class="nl-table">
@@ -253,8 +253,8 @@ jQuery(function($) {
             flow.load({
                     elem: '#flow_'+arg['data_id'] //流加载容器
                     // ,scrollElem:'#flow_'+arg['data_id']
-                    // ,isAuto: false
-                    // ,isLazyimg: true
+                    ,isAuto: false
+                    ,isLazyimg: true
                     ,done: function(page, next){ //加载下一页
                         if(arg['myPage']==1){
                             $('#rank_'+arg['data_id']).empty()
