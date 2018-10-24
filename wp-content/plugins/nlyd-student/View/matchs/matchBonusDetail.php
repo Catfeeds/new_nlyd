50d431fb49e16fec2f92b7ba9eb82291cea51f7b
weihaidong <296656132@qq.com>
2018-10-24 14:15:36 +0800

    修改

--- wp-content/plugins/nlyd-student/View/matchs/matchBonusDetail.php
+++ wp-content/plugins/nlyd-student/View/matchs/matchBonusDetail.php
@@ -1,7 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/10/24
- * Time: 9:03
- */
+<?php require_once PLUGINS_PATH.'nlyd-student/View/public/student-footer-menu.php' ;?>
+<div class="layui-fluid">
+    <div class="layui-row">
+        <?php
+                require_once leo_student_public_view.'leftMenu.php';
+            
+        ?>
+        <div class="nl-right-content layui-col-sm12 layui-col-xs12 layui-col-md12 detail-content-wrapper">
+        <header class="mui-bar mui-bar-nav">
+            <a class="mui-pull-left nl-goback">
+                <div><i class="iconfont">&#xe610;</i></div>
+            </a>
+            <h1 class="mui-title"><div><?=__('成绩', 'nlyd-student')?></div></h1>
+        </header>    
+            <div class="layui-row nl-border nl-content ">
+                <div class="match-title c_black"><?=$match_title?></div>
+               
+            </div>
+        </div>           
+    </div>
+</div>
