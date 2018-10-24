66795f739f626cbfc51cb5f3b4b93776e6a048ff
luoxiao159 <1115996550@qq.com>
2018-10-24 12:03:00 +0800

    10-24 8 计算比赛结束时间match_meta_new

--- wp-content/themes/dt-armada-child/functions.php
+++ wp-content/themes/dt-armada-child/functions.php
@@ -60,7 +60,7 @@ if(!function_exists('get_match_end_time')){
         global $wpdb;
 
         //获取比赛信息
-        $sql = "select match_id,match_start_time,match_more,match_use_time,match_project_interval,match_subject_interval from {$wpdb->prefix}match_meta where match_id = {$match_id}";
+        $sql = "select match_id,match_start_time,match_more,match_use_time,match_project_interval,match_subject_interval from {$wpdb->prefix}match_meta_new where match_id = {$match_id}";
 
         $match = $wpdb->get_row($sql,ARRAY_A);
         if(empty($match)){
