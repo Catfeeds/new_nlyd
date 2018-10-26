d0365f1324ac9620075f77d7c91a03acf205b51b
weihaidong <296656132@qq.com>
2018-10-26 11:45:04 +0800

    签到调整

--- wp-content/plugins/nlyd-student/View/signs/success.php
+++ wp-content/plugins/nlyd-student/View/signs/success.php
@@ -11,36 +11,49 @@
     <div class="layui-row">
         <div class="nl-right-content layui-col-sm12 layui-col-xs12 layui-col-md12 detail-content-wrapper">
             <div class="layui-row nl-border nl-content">
+                <div class="signs_header">
+                    <div class="ta_c c_black fs_16">
+                    亿盖教育2018脑力世界杯总决赛<br>
+                    <span class="fs_12">2018 INTELLECTUAL WORLD CUP FINAL</span>
+                    </div>
+                </div>
                 <div class="signs_box">
                     <div class="img-box right_img">
                         <img src="<?=student_css_url.'image/signs/success.png'?>">
                     </div>
-                    <h3 class="signs_tips"><?=__('恭喜你，签到成功', 'nlyd-student')?></h3>
-                    <p class="signs_title fs_16 c_black"><?=__($match_title, 'nlyd-student')?></p>
-                    <div class="signs_row fs_16">
-                        <span class="signs_label c_black"><?=__('座位号', 'nlyd-student')?>：</span>
-                        <span class="signs_value"><?=$index?></span>
+                    <h3 class="signs_tips c_blue"><?=__('恭喜您，签到成功', 'nlyd-student')?></h3>
+                    <div class="signs_row">
+                        <div class="signs_label"><?=__('参赛座位号', 'nlyd-student')?>：</div>
+                        <div class="signs_value c_blue fs_15">第<?=$index?>号</div>
                     </div>
-                    <div class="signs_row fs_16">
-                        <span class="signs_label c_black"><?=__('选手姓名', 'nlyd-student')?>：</span>
-                        <span class="signs_value"><?=$real_name?></span>
+                    <div class="signs_row">
+                        <div class="signs_label"><?=__('选手国籍', 'nlyd-student')?>：</div>
+                        <div class="signs_value c_black fs_15">中华人民共和国 <span class="fastbannerform__span f32 NOFLAG <?=empty($user_info['user_nationality_pic']) ? 'cn' : $user_info['user_nationality_pic']?>"></span></div>
                     </div>
-                    <div class="signs_row fs_16">
-                        <span class="signs_label c_black"><?=__('证件号码', 'nlyd-student')?>：</span>
-                        <span class="signs_value"><?=$real_ID?></span>
+                    <div class="signs_row">
+                        <div class="signs_label"><?=__('选手姓名', 'nlyd-student')?>&<?=__('证件号码', 'nlyd-student')?>：</div>
+                        <div class="signs_value c_black fs_15"><?=$real_name?><br><?=$real_ID?>(身份证)</div>
                     </div>
-                    <div class="signs_row fs_16">
-                        <span class="signs_label c_black"><?=__('城市信息', 'nlyd-student')?>：</span>
-                        <span class="signs_value"><?=$address?></span>
+                    <div class="signs_row">
+                        <div class="signs_label"><?=__('性别', 'nlyd-student')?>&<?=__('年龄', 'nlyd-student')?>：</div>
+                        <div class="signs_value c_black fs_15">女 1991/03/07 儿童组</div>
                     </div>
-                    <a href="<?=home_url('/matchs');?>" class="go_match"><span><?=__('去赛事中心', 'nlyd-student')?></span></a>
+                    <!-- <div class="signs_row fs_16">
+                        <div class="signs_label"><?=__('证件号码', 'nlyd-student')?>：</div>
+                        <div class="signs_value c_black"><?=$real_ID?></div>
+                    </div> -->
+                    <!-- <div class="signs_row fs_16">
+                        <div class="signs_label"><?=__('城市信息', 'nlyd-student')?>：</div>
+                        <div class="signs_value c_black"><?=$address?></div>
+                    </div> -->
+                    <a href="<?=home_url('/matchs');?>" class="a-btn"><div><?=__('去赛事中心', 'nlyd-student')?></div></a>
                 </div>
 
                 <div class="signs_footer">
                     <div class="img-box logo_img">
                         <img src="<?=student_css_url.'image/nlyd-big.png'?>">
                     </div>
-                    <p class="ta_c fs_16"><?=__('脑力世界杯组委会', 'nlyd-student')?></p>
+                    <p class="ta_c c_black"><?=__('脑力世界杯组委会', 'nlyd-student')?></p>
                 </div>
             </div>
         </div>
