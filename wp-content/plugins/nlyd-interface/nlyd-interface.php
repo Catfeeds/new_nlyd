c0e63ed65b6fa957c9da8718e82c1a3cc0395e18
cd_leo <cd_leo@163.com>
2018-10-26 11:49:03 +0800

    v2.1.0.5

--- wp-content/plugins/nlyd-interface/nlyd-interface.php
+++ wp-content/plugins/nlyd-interface/nlyd-interface.php
@@ -345,6 +345,6 @@ if(!class_exists('myInterface')){
 
 }
 define( 'leo_user_interface_path', plugin_dir_path( __FILE__ ) );
-define( 'leo_user_interface_version','v2.1.0.4' );//样式版本
+define( 'leo_user_interface_version','v2.1.0.5' );//样式版本
 
 new myInterface();
