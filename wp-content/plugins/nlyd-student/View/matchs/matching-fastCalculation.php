7f62a1e576baf747ac1480455ed1af814926e450
weihaidong <296656132@qq.com>
2018-11-05 15:53:11 +0800

    xiugai

--- wp-content/plugins/nlyd-student/View/matchs/matching-fastCalculation.php
+++ wp-content/plugins/nlyd-student/View/matchs/matching-fastCalculation.php
@@ -7,9 +7,9 @@
         </header>
             <div class="layui-row nl-border nl-content">
                 <div class="remember width-margin width-margin-pc">
-                    <!-- <div class="matching-row layui-row">
+                    <div class="matching-row layui-row">
                         <div class="c_black match_info_font"><div><?=__($project_title, 'nlyd-student')?></div></div>
-                    </div> -->
+                    </div>
                     <div class="matching-row layui-row have-submit" style="margin-top:0">
                         <div class="c_black match_info_font"><div><?php printf(__('第%s轮', 'nlyd-student'), $match_more_cn)?></div></div>
                         <div class="c_blue match_info_font"><div><?=__('第<span id="total">0</span>题', 'nlyd-student')?></div></div>
@@ -247,12 +247,26 @@ jQuery(function($) {
             firstNumber+=oneNumber;
         }
         answer=parseInt(firstNumber);
+        var arr=['+','-'];
         for (var index = 0; index < L; index++) {
             var symbol=''
             if(type=='<?=__('连加运算', 'nlyd-student')?>'){
                 symbol='+';
             }else if(type=='<?=__('加减运算', 'nlyd-student')?>'){
-                symbol=randJJ()
+                // symbol=randJJ()
+                if(L==1){//一个符号
+                    symbol="-"
+                }else{//多个符号，前两个为加减
+                    if(index==0){
+                        var pos = Math.round(Math.random() * (arr.length - 1));
+                        symbol=arr[pos]
+                        arr.splice(pos, 1);
+                    }else if (index==1) {
+                        symbol=arr[0]
+                    }else{
+                        symbol=randJJ()
+                    }
+                }
             }
             var number=''
             for (var i = 0; i < N; i++) {
--- wp-content/plugins/nlyd-student/View/matchs/matching-fastReverse.php
+++ wp-content/plugins/nlyd-student/View/matchs/matching-fastReverse.php
@@ -8,9 +8,9 @@
         </header>
             <div class="layui-row nl-border nl-content">
                 <div class="remember width-margin width-margin-pc">
-                    <!-- <div class="matching-row layui-row">
+                    <div class="matching-row layui-row">
                         <div class="c_black match_info_font"><div><?=__($project_title, 'nlyd-student')?></div></div>
-                    </div> -->
+                    </div>
                     <div class="matching-row layui-row have-submit" style="margin-top:0;">
                         <div class="c_black match_info_font"><div><?php printf(__('第%s轮', 'nlyd-student'), $match_more_cn)?></div></div>
                         <div class="c_blue match_info_font"><div><?=__('第<span id="total">0</span>题', 'nlyd-student')?></div></div>
