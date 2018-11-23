3ad64a3ba9025afed9830b88372c8e9d76fe1573
weihaidong <296656132@qq.com>
2018-11-23 17:02:58 +0800

    文章速读

--- wp-content/plugins/nlyd-student/View/gradings/grading-voice.php
+++ wp-content/plugins/nlyd-student/View/gradings/grading-voice.php
@@ -3,7 +3,7 @@
     <div class="layui-row">
         <div class="layui-col-lg12 layui-col-md12 layui-col-sm12 layui-col-xs12 layui-col-md12 detail-content-wrapper">
         <header class="mui-bar mui-bar-nav">
-            <h1 class="mui-title"><div><?=__($project_title, 'nlyd-student')?></div></h1>
+            <h1 class="mui-title"><div><?=__($grading_title, 'nlyd-student')?></div></h1>
         </header>
             <div class="layui-row nl-border nl-content">
 
--- wp-content/plugins/nlyd-student/View/gradings/matching-fastReverse.php
+++ wp-content/plugins/nlyd-student/View/gradings/matching-fastReverse.php
@@ -4,12 +4,12 @@
     <div class="layui-row">
         <div class="layui-col-lg12 layui-col-md12 layui-col-sm12 layui-col-xs12 layui-col-md12 detail-content-wrapper">
         <header class="mui-bar mui-bar-nav">
-            <h1 class="mui-title"><div><?=__($project_title, 'nlyd-student')?></div></h1>
+            <h1 class="mui-title"><div><?=__($grading_title, 'nlyd-student')?></div></h1>
         </header>
             <div class="layui-row nl-border nl-content">
                 <div class="remember width-margin width-margin-pc">
                     <!-- <div class="matching-row layui-row">
-                        <div class="c_black match_info_font"><div><?=__($project_title, 'nlyd-student')?></div></div>
+                        <div class="c_black match_info_font"><div><?=__($grading_title, 'nlyd-student')?></div></div>
                     </div> -->
                     <div class="matching-row layui-row have-submit">
                         <div class="c_black match_info_font"><div><?=__('逆向运算', 'nlyd-student')?></div></div>
