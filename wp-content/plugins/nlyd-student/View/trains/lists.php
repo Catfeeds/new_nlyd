d72b6be4b5c3cffb6adf9faa9164195ccba59704
cd_leo <cd_leo@163.com>
2018-11-28 15:54:55 +0800

    V2.1.2.1

--- wp-content/plugins/nlyd-student/View/trains/lists.php
+++ wp-content/plugins/nlyd-student/View/trains/lists.php
@@ -19,57 +19,69 @@
                         <?php
                         if (empty($list)){ ?>
                             <div class="ta_c"><?=__('暂无专项训练', 'nlyd-student')?></div>
-                            <div class="lists_item_row">
-                                <div class="bold c_black fs_16"><?=__('速读考级水平(自测)', 'nlyd-student')?></div>
-                                <a class="item_row lists_row c_black6"  href="<?=home_url('trains/ready/type/reading/genre_id/755/id/1')?>" >
+                            <!--<div class="lists_item_row">
+                                <div class="bold c_black fs_16"><?/*=__('速读考级水平(自测)', 'nlyd-student')*/?></div>
+                                <a class="item_row lists_row c_black6"  href="<?/*=home_url('trains/ready/type/reading/genre_id/755/id/1')*/?>" >
                                     <div class="item_img"><img src=""></div>
-                                    <div class="name_wrapper dis_table"><div class="dis_cell"><?=__('速读', 'nlyd-student')?></div></div>
-                                    <div class="arrow_box"><img src="<?=student_css_url.'image/trains/arrow.png'?>"></div>
+                                    <div class="name_wrapper dis_table"><div class="dis_cell"><?/*=__('速读', 'nlyd-student')*/?></div></div>
+                                    <div class="arrow_box"><img src="<?/*=student_css_url.'image/trains/arrow.png'*/?>"></div>
                                 </a>
                             </div>
                             <div class="lists_item_row">
-                                <div class="bold c_black fs_16"><?=__('速记考级水平(自测)', 'nlyd-student')?></div>
-                                <a class="item_row lists_row c_black6"  href="<?=home_url('trains/ready/type/memory/genre_id/755/id/1')?>" >
+                                <div class="bold c_black fs_16"><?/*=__('速记考级水平(自测)', 'nlyd-student')*/?></div>
+                                <a class="item_row lists_row c_black6"  href="<?/*=home_url('trains/ready/type/memory/genre_id/755/id/1')*/?>" >
                                     <div class="item_img"><img src=""></div>
-                                    <div class="name_wrapper dis_table"><div class="dis_cell"><?=__('速记', 'nlyd-student')?></div></div>
-                                    <div class="arrow_box"><img src="<?=student_css_url.'image/trains/arrow.png'?>"></div>
+                                    <div class="name_wrapper dis_table"><div class="dis_cell"><?/*=__('速记', 'nlyd-student')*/?></div></div>
+                                    <div class="arrow_box"><img src="<?/*=student_css_url.'image/trains/arrow.png'*/?>"></div>
                                 </a>
                             </div>
                             <div class="lists_item_row">
-                                <div class="bold c_black fs_16"><?=__('速算考级水平(自测)', 'nlyd-student')?></div>
-                                <a class="item_row lists_row c_black6"  href="<?=home_url('trains/ready/type/arithmetic/genre_id/755/id/1')?>" >
+                                <div class="bold c_black fs_16"><?/*=__('速算考级水平(自测)', 'nlyd-student')*/?></div>
+                                <a class="item_row lists_row c_black6"  href="<?/*=home_url('trains/ready/type/arithmetic/genre_id/755/id/1')*/?>" >
                                     <div class="item_img"><img src=""></div>
-                                    <div class="name_wrapper dis_table"><div class="dis_cell"><?=__('速算', 'nlyd-student')?></div></div>
-                                    <div class="arrow_box"><img src="<?=student_css_url.'image/trains/arrow.png'?>"></div>
+                                    <div class="name_wrapper dis_table"><div class="dis_cell"><?/*=__('速算', 'nlyd-student')*/?></div></div>
+                                    <div class="arrow_box"><img src="<?/*=student_css_url.'image/trains/arrow.png'*/?>"></div>
                                 </a>
-                            </div>
-                        <?php }else{ ?>
-                            <?php foreach ($list as $v){ ?>
-                                <div class="lists_item_row">
-                                    <div class="bold c_black fs_16"><?=__($v["title"], 'nlyd-student')?></div>
-                                    <?php foreach ($v["children"] as $val){ ?>
-                                        <a class="item_row lists_row c_black6"  href="<?=home_url('trains/ready/type/'.$val->project_alias.'/genre_id/'.$genre_id.'/id/'.$val->ID)?>" >
-                                            <?php
-                                            $thumbnail_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($val->ID), 'thumbnail');
-                                            ?>
-                                            <div class="item_img"><img src="<?=$thumbnail_image_url[0]?>"></div>
-                                            <div class="name_wrapper dis_table"><div class="dis_cell"><?=__($val->post_title, 'nlyd-student')?></div></div>
-                                            <div class="arrow_box"><img src="<?=student_css_url.'image/trains/arrow.png'?>"></div>
-                               
-                                        </a>
-                                    <?php }?>
-                                </div>
-                            <?php }?>
-                        <?php }?>
+                            </div>-->
+                        <?php }else{
+                                if($project_alias == 'grading'){
+                                    foreach ($list as $k =>$v){
+                                        //print_r($k);
+                                        $alias = get_post_meta($k,'project_alias')[0];
+                                    ?>
+                                    <div class="bold c_black fs_16">
+                                        <a href="<?=home_url('/grade/matchWaitting/grad_type/'.$alias)?>"><?=__($v["title"], 'nlyd-student')?></a>
+                                    </div>
+                        <?php       }
+                                }else{
+                                     foreach ($list as $v){ ?>
+                                        <div class="lists_item_row">
+                                            <div class="bold c_black fs_16"><?=__($v["title"], 'nlyd-student')?></div>
+                                            <?php foreach ($v["children"] as $val){ ?>
+                                                <a class="item_row lists_row c_black6"  href="<?=home_url('trains/ready/type/'.$val->project_alias.'/genre_id/'.$genre_id.'/id/'.$val->ID)?>" >
+                                                    <?php
+                                                    $thumbnail_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($val->ID), 'thumbnail');
+                                                    ?>
+                                                    <div class="item_img"><img src="<?=$thumbnail_image_url[0]?>"></div>
+                                                    <div class="name_wrapper dis_table"><div class="dis_cell"><?=__($val->post_title, 'nlyd-student')?></div></div>
+                                                    <div class="arrow_box"><img src="<?=student_css_url.'image/trains/arrow.png'?>"></div>
+
+                                                </a>
+                                            <?php }?>
+                                        </div>
+                                    <?php }
+                                }
+                        } ?>
                     </div>
 
-
+<?php if($project_alias == 'mental_world_cup'): ?>
 <div class="width-padding width-padding-pc lists_row">
 <span class="fs_12">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;脑力世界杯是国际脑力运动委员会（IISC）发起的一项世界顶级综合性脑力赛事，是比拼选手脑力素质的世界舞台，六大标准项目分别从不同侧面综合考查选手注意力、感知力、理解力、记忆力和创造力五大脑力素质，是全球最全面而精炼的脑力大赛。</span>
 <br><span class="fs_12">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Intellectual World Cup is a world top comprehensive intellectual competition sponsored by the International Intellectual Sports Commission (IISC). It is a world stage for competitors to show  their brain. The six events comprehensively examine the five intellectual qualities of competitors from different aspects: attention, perception, comprehension, memory and creativity. It is the most comprehensive and refined mental contest in the world.</span>
 <br><span class="fs_12">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;本版块供选手针对该赛事各项目开展自我训练，相关挑战指标可根据自身水平自行设定，循序渐进，以逐步提高实战能力。</span>
 <br><span class="fs_12">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This section for the athletes to carry out self-training for the events, the users can set different time and difficulty by themself to gradually improve their actual ability.</span>
 </div>
+<?php endif;?>
                 </div>
             </div>
         </div>
