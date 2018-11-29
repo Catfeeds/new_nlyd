9f82fc0e991121c1c6447bdbdbe3c143d2ef5da8
weihaidong <296656132@qq.com>
2018-11-29 11:14:36 +0800

    xiugai

--- wp-content/plugins/nlyd-student/View/trains/lists.php
+++ wp-content/plugins/nlyd-student/View/trains/lists.php
@@ -52,7 +52,7 @@
                                     <div class="lists_item_row">
                                         <div class="bold c_black fs_16"></div>
                                         <a class="item_row lists_row c_black6 kaoji"  href="<?=home_url('trains/ready/genre_id/'.$genre_id.'/type/'.$alias)?>" >
-                                            <div class="item_img"><img src="<?=$thumbnail_image_url[0]?>"></div>
+                                            <div class="item_img"><img src="<?=student_css_url.'image/trains/arrow.png'?>"></div>
                                             <div class="name_wrapper dis_table"><div class="dis_cell"><?=__($v["title"], 'nlyd-student')?></div></div>
                                             <div class="arrow_box"><img src="<?=student_css_url.'image/trains/arrow.png'?>"></div>
                                         </a>
