7b5fa2283c8b003ae04e7cce6d8224a908e442e7
weihaidong <296656132@qq.com>
2018-12-03 16:21:44 +0800

    xiugai

--- wp-content/plugins/nlyd-student/View/orders/order-list.php
+++ wp-content/plugins/nlyd-student/View/orders/order-list.php
@@ -1,12 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: leo
- * Date: 2018/7/16
- * Time: 17:41
- */
-
-?>
 <div class="layui-fluid">
     <div class="layui-row">
         <div class="nl-right-content layui-col-sm12 layui-col-xs12 detail-content-wrapper">
@@ -14,7 +5,7 @@
                 <a class="mui-pull-left nl-goback">
                     <div><i class="iconfont">&#xe610;</i></div>
                 </a>
-                <h1 class="mui-title"><div>我的订单</div></h1>
+                <h1 class="mui-title"><div><?=__('我的订单', 'nlyd-student')?></div></h1>
             </header>
             <?php if($row){?>
 
@@ -32,20 +23,20 @@
                         <div class="search-warapper">
                             <div class="search-zoo">
                                 <i class="iconfont search-Icon">&#xe63b;</i>
-                                <input type="text" class="serach-Input" placeholder="搜索商品">
+                                <input type="text" class="serach-Input" placeholder="<?=__('搜索商品', 'nlyd-student')?>">
                             </div>
                         </div>
                         <div class="layui-tab layui-tab-brief" lay-filter="tabs">
                             <ul style="margin-left: 0" class="layui-tab-title">
-                                <li class="layui-this" data-id="all" pay_status="10">全部</li>
-                                <li data-id="apply" pay_status="1">待支付</li>
-                                <li data-id="send" pay_status="2">待发货</li>
-                                <li data-id="accept" pay_status="3">待收货</li>
-                                <div class="nl-transform">全部</div>
+                                <li class="layui-this" data-id="10"><?=__('全部', 'nlyd-student')?></li>
+                                <li data-id="1"><?=__('待支付', 'nlyd-student')?></li>
+                                <li data-id="2"><?=__('待发货', 'nlyd-student')?></li>
+                                <li data-id="3"><?=__('待收货', 'nlyd-student')?></li>
+                                <div class="nl-transform"><?=__('全部', 'nlyd-student')?></div>
                             </ul>
                             <div class="layui-tab-content">
                                 <!-- 全部 -->
-                                <div class="layui-tab-item layui-show flow-default" id="all">
+                                <div class="layui-tab-item layui-show flow-default" id="10">
                                     <!-- <div class="order-row layui-row">
                                         <div class="order-title layui-row width-padding width-padding-pc">
                                             <span class="pull-left">完成时间：2017-07-24 13:20</span>
@@ -104,7 +95,7 @@
                                     </div> -->
                                 </div> 
                                 <!-- 待支付 -->
-                                <div class="layui-tab-item flow-default" id="apply">
+                                <div class="layui-tab-item flow-default" id="1">
                                     <!-- <div class="order-row layui-row">
                                         <div class="order-title layui-row width-padding width-padding-pc">
                                             <span class="pull-left">创建时间：2017-07-24 13:20</span>
@@ -164,7 +155,7 @@
                                     </div> -->
                                 </div> 
                                 <!-- 代发货 -->
-                                <div class="layui-tab-item flow-default" id="send">
+                                <div class="layui-tab-item flow-default" id="2">
                                     <!-- <div class="order-row layui-row">
                                         <div class="order-title layui-row width-padding width-padding-pc">
                                             <span class="pull-left">支付时间：2017-07-24 13:20</span>
@@ -224,7 +215,7 @@
                                     </div> -->
                                 </div> 
                                 <!-- 待收货 -->
-                                <div class="layui-tab-item flow-default" id="accept">
+                                <div class="layui-tab-item flow-default" id="3">
                                     <!-- <div class="order-row layui-row">
                                         <div class="order-title layui-row width-padding width-padding-pc">
                                             <span class="pull-left">发货时间：2017-07-24 13:20</span>
@@ -270,8 +261,8 @@ jQuery(function($) {
 layui.use(['element','flow'], function(){
     var element = layui.element; //Tab的切换功能，切换事件监听等，需要依赖element模块
     var flow = layui.flow;//流加载
-    var isClick={all:true,apply:false,send:false,accept:false}
-    function pagation(data_id,pay_status) {
+    var isClick={'10':true,'1':false,'2':false,'3':false}
+    function pagation(data_id) {
         flow.load({
                 elem: '#'+data_id
                 ,isAuto: false
@@ -280,9 +271,10 @@ layui.use(['element','flow'], function(){
                     var postData={
                         action:'getOrderList',
                         page:page,
-                        pay_status:pay_status,//10全部,1待支付,2待发货,3待收货,4已完成,-1待退款,-2已退款
+                        pay_status:data_id,//10全部,1待支付,2待发货,3待收货,4已完成,-1待退款,-2已退款
                     }
                     var lis = [];
+                    console.log(postData)
                     $.ajax({
                         data:postData
                         ,success:function(res,ajaxStatu,xhr){  
@@ -290,18 +282,32 @@ layui.use(['element','flow'], function(){
                             isClick[data_id]=true
                             if(res.success){
                                 $.each(res.data.info,function(i,v){
-
+                                    var list_info={}
                                     if(v.pay_status=="1"){//1待支付
-
+                                        list_info['time']="<?=__('创建时间', 'nlyd-student')?>";
+                                        list_info['statu_color']="c_orange";
+                                        list_info['btn_html']='<div href="" class="order-right-btn c_blue"><?=__('支付订单', 'nlyd-student')?></div>'
+                                                                +'<div class="order-left-btn"><?=__('取消订单', 'nlyd-student')?></div>';
                                     }else if(v.pay_status=="2"){//2待发货
-
+                                        list_info['time']="<?=__('支付时间', 'nlyd-student')?>";
+                                        list_info['statu_color']="c_blue";
+                                        list_info['btn_html']='<a href="<?=home_url('orders/details')?>" class="order-right-btn c_blue"><?=__('查看详情', 'nlyd-student')?></a>'
+                                                            +'<div class="order-left-btn"><?=__('提醒发货', 'nlyd-student')?></div>';
                                     }else if(v.pay_status=="3"){//3待收货
-
+                                        list_info['time']="<?=__('发货时间', 'nlyd-student')?>";
+                                        list_info['statu_color']="c_blue"
+                                        list_info['btn_html']='<a href="" class="order-right-btn c_blue"><?=__('确认收货', 'nlyd-student')?></a>'
+                                                            +'<a  href="<?=home_url('orders/logistics')?>" class="order-left-btn ml-20"><?=__('查看物流', 'nlyd-student')?></a>'
+                                                            +'<a class="order-left-btn" href="<?=home_url('orders/details')?>"><?=__('查看详情', 'nlyd-student')?></a>';
+                                    }else if(v.pay_status=="4"){//4已完成
+                                        list_info['time']="<?=__('完成时间', 'nlyd-student')?>";
+                                        list_info['statu_color']="c_black6"
+                                        list_info['btn_html']='<a class="order-left-btn" href="<?=home_url('orders/details')?>"><?=__('查看详情', 'nlyd-student')?></a>';
                                     }
                                     var dom='<div class="order-row layui-row">'
                                                 +'<div class="order-title layui-row width-padding width-padding-pc">'
-                                                    +'<span class="pull-left">发货时间：2017-07-24 13:20</span>'
-                                                    +'<span class="pull-right blue">待收货</span>'
+                                                    +'<span class="pull-left">'+list_info['time']+'：'+v.created_time+'</span>'
+                                                    +'<span class="pull-right '+list_info['statu_color']+'">'+v.pay_status_title+'</span>'
                                                 +'</div>'
                                                 +'<div class="order-body layui-row  width-padding width-padding-pc">'
                                                     +'<div class="order-body-top layui-row">'
@@ -313,18 +319,16 @@ layui.use(['element','flow'], function(){
                                                             +'<p class="order-content">脑力世界杯是一年一度的国际大赛，汇聚了海内外很多脑力健将</p>'
                                                         +'</div>'
                                                         +'<div class="order-right-info  pull-left">'
-                                                            +'<p class="order-price">￥380.00</p>'
+                                                            +'<p class="order-price">￥'+v.cost+'</p>'
                                                             +'<p class="order-price">x1</p>'
                                                         +'</div>'
                                                     +'</div>'
                                                     +'<p class="order-body-bottom">'    
-                                                        +'共1件商品 实际支付: ￥155.00'
+                                                        +'共1件商品 实际支付: ￥'+v.cost
                                                     +'</p>'
                                                 +'</div>'
                                                 +'<div class="order-footer layui-row width-padding width-padding-pc">'
-                                                    +'<a href="" class="order-right-btn blue">确认收货</a>'
-                                                    +'<div class="order-left-btn ml-20">查看物流</div>'
-                                                    +'<div class="order-left-btn">查看详情</div>'
+                                                +list_info['btn_html']
                                                 +'</div>'
                                             +'</div>'
                                     lis.push(dom) 
@@ -343,12 +347,26 @@ layui.use(['element','flow'], function(){
                 }
             });
     }
-    pagation("all",10)
+    
+    var layid = location.hash.replace(/^#matchList=/, '');
+    pagation(layid)
+        if(layid.length>0){
+            $('.layui-tab-title li').each(function(){
+                var _this=$(this)
+                var lay_id=_this.attr('data-id');
+                if(lay_id==layid){
+                    setTimeout(function() {
+                        _this.click()
+                    }, 200);
+                    return false
+                }
+            })
+        }
     element.on('tab(tabs)', function(){//tabs
+        location.hash = 'matchList='+ $(this).attr('data-id');
         var left=$(this).position().left+parseInt($(this).css('marginLeft'));
         var html=$(this).html();
         var data_id=$(this).attr('data-id')
-        var pay_status=$(this).attr('pay_status')
         $('.nl-transform').css({
             'transform':'translate3d('+left+'px, 0px, 0px)'
         }).html(html)
---
+++ wp-content/plugins/nlyd-student/View/orders/order-logistics.php
@@ -0,0 +1,58 @@
+<style>
+@media screen and (max-width: 1199px){
+    #page{
+        background-color:#f6f6f6!important;
+    }
+}
+</style>
+<div class="layui-fluid">
+    <div class="layui-row">
+        <div class="nl-right-content layui-col-sm12 layui-col-xs12 detail-content-wrapper">
+            <header class="mui-bar mui-bar-nav">
+                <a class="mui-pull-left nl-goback">
+                    <div><i class="iconfont">&#xe610;</i></div>
+                </a>
+                <h1 class="mui-title"><div><?=__('物流信息', 'nlyd-student')?></div></h1>
+            </header>
+            <div class="layui-row nl-border nl-content">
+               <div class="layui-row width-padding width-padding-pc goods_row">
+                    <div class="order-img img-box pull-left">
+                        <img src="<?=student_css_url.'image/noInfo/noOrder1096@2x.png'?>">
+                    </div>
+                    <div class="order-detail pull-left">
+                        <p class="order-name">2018脑力世界杯总决赛（重庆）报名</p>
+                        <p class="order-content">脑力世界杯是一年一度的国际大赛，汇聚了海内外很多脑力健将</p>
+                    </div>
+                    <div class="order-right-info  pull-left">
+                        <p class="order-price">￥0.00</p><p class="order-price">x1</p>
+                    </div>
+                </div>
+                <div class="layui-row width-padding width-padding-pc goods_row">
+                    <div class="logistics_step active">
+                        <div class="circle_dot">●</div>
+                        <div class="logistics_name">订单交易成功</div>
+                        <div class="logistics_time">2018-07-02 20:00:05</div>
+                    </div>
+                    <div class="logistics_step">
+                        <div class="circle_dot">●</div>
+                        <div class="logistics_name">系统自动确认收货</div>
+                        <div class="logistics_time">2018-07-02 20:00:05</div>
+                    </div>
+                    <div class="logistics_step">
+                        <div class="circle_dot">●</div>
+                        <div class="logistics_name">【成都市】已签收，签收人：聚点社区已签收,感谢使用圆通速递，期待再次为您服务</div>
+                        <div class="logistics_time">2018-07-02 20:00:05</div>
+                    </div>
+                </div>
+            </div>
+        </div>  
+
+    </div>
+</div>
+
+<script>
+jQuery(function($) {
+
+
+})
+</script>
