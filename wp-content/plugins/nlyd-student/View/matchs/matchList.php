5c257cd3e6a0a8f504e74deeeb694d45304da99f
cd_leo <cd_leo@163.com>
2018-12-07 11:03:15 +0800

    V2.1.3.2

--- wp-content/plugins/nlyd-student/View/matchs/matchList.php
+++ wp-content/plugins/nlyd-student/View/matchs/matchList.php
@@ -12,8 +12,20 @@
                 <div class="layui-row nl-border nl-content">
                     <div class="layui-tab layui-tab-brief" lay-filter="tabs" style="margin:0">
                         <ul style="margin-left:0;padding:0" class="mui-bar mui-bar-nav layui-tab-title">
-                            <li class="layui-this" data-id="1" lay-id="1"><?=__('报名中', 'nlyd-student')?></li>
-                            <li data-id="2" lay-id="2"><?=__('比赛中', 'nlyd-student')?></li>
+                            <li class="layui-this" data-id="1" lay-id="1"><?=__('报名中', 'nlyd-student')?>
+                                <?php
+                                    if($entry_is_true > 0){
+                                        echo '('.$entry_is_true.')';
+                                    }
+                                ?>
+                            </li>
+                            <li data-id="2" lay-id="2"><?=__('比赛中', 'nlyd-student')?>
+                                <?php
+                                if($match_is_true > 0){
+                                    echo '('.$match_is_true.')';
+                                }
+                                ?>
+                            </li>
                             <li data-id="3" lay-id="3"><?=__('往期比赛', 'nlyd-student')?></li>
                             <div class="nl-transform" data-y="-5"><?=__('近期比赛', 'nlyd-student')?></div>
                         </ul>
