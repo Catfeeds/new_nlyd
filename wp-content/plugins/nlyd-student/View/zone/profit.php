c9e89427ed35caa1423f65eed586fb14e90aef4c
weihaidong <296656132@qq.com>
2018-12-15 17:41:41 +0800

    1

--- wp-content/plugins/nlyd-student/View/zone/profit.php
+++ wp-content/plugins/nlyd-student/View/zone/profit.php
@@ -138,6 +138,81 @@ jQuery(function($) {
     layui.use(['element','flow'], function(){
         var element = layui.element; //Tab的切换功能，切换事件监听等，需要依赖element模块
         var flow = layui.flow;//流加载
-    })
+        function pagation(id,profit_page){
+            flow.load({
+                elem: '#'+id //流加载容器
+                ,isAuto: false
+                ,isLazyimg: true
+                ,done: function(page, next){ //加载下一页
+                    var postData={
+                        action:'get_user_profit_logs',
+                        page:profit_page,
+                    }
+                    if(parseInt(id)==1){//全部
+                        postData['map']="all";
+                    }else if(parseInt(id)==3){//提现
+                        postData['map']="extract";
+                    }else{//收益
+                        postData['map']="";
+                    }
+                    var lis = [];
+                    $.ajax({
+                        data: postData,
+                        success:function(res,ajaxStatu,xhr){
+                            console.log(res)
+                            profit_page++
+                            isClick[id]=true
+                            if(res.success){
+                                $.each(res.data.info,function(i,v){
+                                   
+                                    var dom='';
+                                    lis.push(dom) 
+                                })
+                                if (res.data.info.length<50) {
+                                    next(lis.join(''),false) 
+                                }else{
+                                    next(lis.join(''),true) 
+                                }
+                                
+                            }else{
+                                next(lis.join(''),false)
+                            }
+                        },
+                        complete:function(XMLHttpRequest, textStatus){
+							if(textStatus=='timeout'){
+								$.alerts('<?=__('网络质量差,请重试', 'nlyd-student')?>')
+								next(lis.join(''),true)
+							}
+                        }
+                    })       
+                }
+            });
+        }
+        var isClick={}
+    //    if(location.hash.replace(/^#profit=/, '').length==0){
+    //        //获取hash来切换选项卡，假设当前地址的hash为lay-id对应的值
+    //        location.hash = 'profit='+ <?=$anchor?>;
+    //    }
+        // var layid = location.hash.replace(/^#profit=/, '');
+     
+        // element.tabChange('profit', layid);
+        pagation($('.layui-this').attr('lay-id'),1)
+        var lefts=$('.layui-this').position().left;
+        $('.nl-transform').css({
+            'transform':'translate3d('+lefts+'px, -5px, 0px)'
+        })
+        element.on('tab(profit)', function(){//profit
+            // location.hash = 'profit='+ $(this).attr('lay-id');
+            var left=$(this).position().left+parseInt($(this).css('marginLeft'));
+            var id=$(this).attr('lay-id')
+            $('.nl-transform').css({
+                'transform':'translate3d('+left+'px, 0px, 0px)'
+            })
+            // if(!isClick[id]){
+            //     pagation(id,1)
+            // }
+        });
+
+    });
 })
 </script>
