30e571911a60b8287b034c6b767fad2f2fb98707
weihaidong <296656132@qq.com>
2018-12-28 17:43:29 +0800

    11

--- wp-content/plugins/nlyd-student/View/zone/index-user.php
+++ wp-content/plugins/nlyd-student/View/zone/index-user.php
@@ -16,7 +16,7 @@
             
         ?>
         <div class="nl-right-content layui-col-sm12 layui-col-xs12 layui-col-md12 detail-content-wrapper">
-            <a class="mui-pull-left nl-goback">
+            <a class="mui-pull-left nl-goback static" href="<?=home_url('/account/');?>">
                 <div><i class="iconfont">&#xe610;</i></div>
             </a>
             <div class="layui-row nl-border nl-content">
