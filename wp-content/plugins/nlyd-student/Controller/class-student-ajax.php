850b950171fd220dad1664e2e966a00d47ce98eb
cd_leo <cd_leo@163.com>
2019-01-03 09:51:46 +0800

    V2.1.4.0

--- wp-content/plugins/nlyd-student/Controller/class-student-ajax.php
+++ wp-content/plugins/nlyd-student/Controller/class-student-ajax.php
@@ -5371,10 +5371,10 @@ class Student_Ajax
                 $res = $wpdb->get_results($sql_,ARRAY_A);
                 $user_info = array_column($res,'meta_value','meta_key');
                 //print_r($user_info);
-                $rows[$k]['user_gender'] = !empty($user_info['user_gender']) ? $user_info['user_gender'] : '-' ;
                 $rows[$k]['user_ID'] = !empty($user_info['user_ID']) ? $user_info['user_ID'] : $v['coach_id']+10000000;
-                $user_real_name = unserialize($user_info['user_real_name'])['real_name'];
-                $rows[$k]['real_name'] = !empty($user_real_name) ? $user_real_name : '-' ;
+                $user_real_name = unserialize($user_info['user_real_name']);
+                $rows[$k]['real_name'] = !empty($user_real_name['real_name']) ? $user_real_name['real_name'] : '-' ;
+                $rows[$k]['user_gender'] = !empty($user_real_name['real_age']) ? $user_real_name['real_age'] : '-' ;
             }
         }
         //print_r($rows);
