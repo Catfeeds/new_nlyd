9d3b59d9ddd07de831864cc4b5605dd515a95217
weihaidong <296656132@qq.com>
2019-01-08 16:48:17 +0800

    1

--- wp-content/plugins/nlyd-student/View/zone/apply.php
+++ wp-content/plugins/nlyd-student/View/zone/apply.php
@@ -7,7 +7,7 @@
         ?>
         <div class="nl-right-content layui-col-sm12 layui-col-xs12 layui-col-md12 detail-content-wrapper have-bottom">
             <header class="mui-bar mui-bar-nav">
-                <a class="mui-pull-left nl-goback">
+                <a class="mui-pull-left nl-goback static" href="<?=home_url('/zone/indexUser/');?>">
                     <div><i class="iconfont">&#xe610;</i></div>
                 </a>
                 <h1 class="mui-title"><div><?=__($zone_type_name.'资料填写', 'nlyd-student')?></div></h1>
