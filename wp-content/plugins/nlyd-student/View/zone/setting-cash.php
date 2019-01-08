6f787fafeb194c02f216bb6176d8a3d981061851
weihaidong <296656132@qq.com>
2019-01-08 10:37:43 +0800

    1

--- wp-content/plugins/nlyd-student/View/zone/setting-cash.php
+++ wp-content/plugins/nlyd-student/View/zone/setting-cash.php
@@ -12,30 +12,30 @@
                 <h1 class="mui-title"><div><?=__('关联账号设置', 'nlyd-student')?></div></h1>
             </header>
             <div class="layui-row nl-border nl-content">
-                <div class="apply width-padding layui-row layui-bg-white width-padding-pc">
+                <div class="apply width-padding layui-row layui-bg-white width-padding-pc" style="margin-top:0">
                     <a class="apply_list c_black layui-row" href="<?=home_url('/zone/settingCashCard');?>">
-                        <div class="apply_list_line pull-left ">
+                        <div class="apply_list_line left_bg">
                             <div class="zone_bg bg_bankCard"></div>
                         </div>
-                        <div class="apply_list_line center"><?=__('提现银行卡', 'nlyd-student')?></div>
-                        <div class="apply_list_line pull-right"><i class="iconfont fs_20">&#xe727;</i></div>
-                        <div class="apply_list_line pull-right c_black3 mr_10"><?=__('民生银行 尾号2630 储蓄卡', 'nlyd-student')?></div>
+                        <div class="apply_list_line center left_label"><?=__('提现银行卡', 'nlyd-student')?></div>
+                        <div class="apply_list_line c_black3 right_tips"><?=__('民生银行 尾号2630 储蓄卡', 'nlyd-student')?></div>
+                        <div class="apply_list_line right_icoin"><i class="iconfont fs_20">&#xe727;</i></div>
                     </a>
                     <a class="apply_list c_black layui-row" href="<?=home_url('/zone/settingCashWechat/');?>">
-                        <div class="apply_list_line pull-left">
+                        <div class="apply_list_line left_bg">
                             <div class="zone_bg bg_wechat_qr"></div>
                         </div>
-                        <div class="apply_list_line center"><?=__('收款二维码', 'nlyd-student')?></div>
-                        <div class="apply_list_line pull-right"><i class="iconfont fs_20">&#xe727;</i></div>
-                        <div class="apply_list_line pull-right c_black3 mr_10"><?=__('已设置', 'nlyd-student')?></div>
+                        <div class="apply_list_line center left_label"><?=__('收款二维码', 'nlyd-student')?></div>
+                        <div class="apply_list_line c_black3 right_tips"><?=__('已设置', 'nlyd-student')?></div>
+                        <div class="apply_list_line right_icoin"><i class="iconfont fs_20">&#xe727;</i></div>
                     </a>
                     <a class="apply_list c_black layui-row" href="<?=home_url('/zone/settingCashAlipay/');?>">
-                        <div class="apply_list_line pull-left">
+                        <div class="apply_list_line left_bg">
                             <div class="zone_bg bg_alipay_qr"></div>
                         </div>
-                        <div class="apply_list_line center"><?=__('收款二维码', 'nlyd-student')?></div>
-                        <div class="apply_list_line pull-right"><i class="iconfont fs_20">&#xe727;</i></div>
-                        <div class="apply_list_line pull-right c_black3 mr_10"><?=__('未设置', 'nlyd-student')?></div>
+                        <div class="apply_list_line center left_label"><?=__('收款二维码', 'nlyd-student')?></div>
+                        <div class="apply_list_line c_black3 right_tips"><?=__('未设置', 'nlyd-student')?></div>
+                        <div class="apply_list_line right_icoin"><i class="iconfont fs_20">&#xe727;</i></div>
                     </a>
                 </div>
         </div>            
