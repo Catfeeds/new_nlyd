eb3780b0bbd487579fc7ad1c02043e0a4612f3e0
weihaidong <296656132@qq.com>
2019-01-11 15:34:00 +0800

    1

--- wp-content/plugins/nlyd-student/View/zone/apply-trains.php
+++ wp-content/plugins/nlyd-student/View/zone/apply-trains.php
@@ -63,24 +63,25 @@
                     <div class="input_row"><input class="radius_input_row nl-foucs" type="text" name="legal_person" lay-verify="required" autocomplete="off" placeholder="<?=__('法定代表人姓名', 'nlyd-student')?>" value="<?=!empty($row) ? $row['legal_person'] :''?>"></div>
                 </div>
                 <div>
-                    <div class="lable_row"><span class="c_black"><?=__('对公账户开户名称', 'nlyd-student')?>：</span></div>
-                    <div class="input_row"><input class="radius_input_row nl-foucs" type="text" name="bank_card_name" lay-verify="required" autocomplete="off" placeholder="<?=__('输入对公账户开户名称', 'nlyd-student')?>" value="<?=!empty($row) ? $row['bank_card_name'] :''?>"></div>
-                </div>
-                <div>
-                    <div class="lable_row"><span class="c_black"><?=__('选择对公账户开户行', 'nlyd-student')?>：</span></div>
+                    <div class="lable_row"><span class="c_black"><?=__('对公账户开户行', 'nlyd-student')?>：</span></div>
                     <div class="input_row">
                         <span class="input_row_arrow"><i class="iconfont">&#xe656;</i></span>
                         <input class="radius_input_row nl-foucs" type="text" readonly id="opening_bank" name="opening_bank"  lay-verify="required" autocomplete="off" placeholder="<?=__('选择对公账户开户行', 'nlyd-student')?>" value="<?=!empty($row) ? $row['opening_bank'] :''?>">
                     </div>
                 </div>
                 <div>
-                    <div class="lable_row"><span class="c_black"><?=__('对公账户银行卡号', 'nlyd-student')?>：</span></div>
-                    <div class="input_row"><input class="radius_input_row nl-foucs" type="tel" name="bank_card_num" lay-verify="required" autocomplete="off" placeholder="<?=__('输入对公账户号码', 'nlyd-student')?>" value="<?=!empty($row) ? $row['bank_card_num'] :''?>"></div>
-                </div>
-                <div>
-                    <div class="lable_row"><span class="c_black"><?=__('开户详细地址', 'nlyd-student')?>：</span></div>
+                    <div class="lable_row"><span class="c_black"><?=__('对公账户开户详细地址', 'nlyd-student')?>：</span></div>
                     <div class="input_row"><input class="radius_input_row nl-foucs" type="text" name="opening_bank_address" lay-verify="required" autocomplete="off" placeholder="<?=__('输入对公账户详细开户地址', 'nlyd-student')?>" value="<?=!empty($row) ? $row['opening_bank_address'] :''?>"></div>
                 </div>
+                <div>
+                    <div class="lable_row"><span class="c_black"><?=__('对公账户开户名称', 'nlyd-student')?>：</span></div>
+                    <div class="input_row"><input class="radius_input_row nl-foucs" type="text" name="bank_card_name" lay-verify="required" autocomplete="off" placeholder="<?=__('输入对公账户开户名称', 'nlyd-student')?>" value="<?=!empty($row) ? $row['bank_card_name'] :''?>"></div>
+                </div>
+                <div>
+                    <div class="lable_row"><span class="c_black"><?=__('对公账户开户号码', 'nlyd-student')?>：</span></div>
+                    <div class="input_row"><input class="radius_input_row nl-foucs" type="tel" name="bank_card_num" lay-verify="required" autocomplete="off" placeholder="<?=__('输入对公账户号码', 'nlyd-student')?>" value="<?=!empty($row) ? $row['bank_card_num'] :''?>"></div>
+                </div>
+
                 <div>
                     <div class="lable_row">
                         <span class="c_black"><?=__('分中心总经理', 'nlyd-student')?>：</span>
