7615032a2e8452a03817b2db5e890f4ae4017081
cd_leo <cd_leo@163.com>
2019-01-14 15:23:06 +0800

    V2.1.5.0

--- wp-content/plugins/nlyd-interface/nlyd-interface.php
+++ wp-content/plugins/nlyd-interface/nlyd-interface.php
@@ -345,6 +345,6 @@ if(!class_exists('myInterface')){
 
 }
 define( 'leo_user_interface_path', plugin_dir_path( __FILE__ ) );
-define( 'leo_user_interface_version','VV2.1.4.9' );//样式版本
+define( 'leo_user_interface_version','V2.1.5.0' );//样式版本
 
 new myInterface();
