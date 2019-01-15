ab9edcf537ca5d0cee306c94106db5f760103a40
luoxiao159 <1115996550@qq.com>
2019-01-15 15:27:00 +0800

    1-15 4

--- wp-content/plugins/nlyd-match/nlyd-match.php
+++ wp-content/plugins/nlyd-match/nlyd-match.php
@@ -938,9 +938,11 @@ if(!class_exists('MatchController')){
                     $zone_name = $wpdb->get_row("SELECT zm.zone_city,zm.zone_match_type,zm.zone_name,zm.type_id FROM {$wpdb->prefix}team_meta AS tm 
                                  LEFT JOIN {$wpdb->prefix}zone_meta AS zm ON zm.user_id=tm.user_id
                                  WHERE tm.team_id='{$id}' AND tm.user_id>0",ARRAY_A);
-                    $type_alias = $wpdb->get_var("SELECT zone_type_alias FROM {$wpdb->prefix}zone_type WHERE id={$zone_name['type_id']}");
-                    $organizeClass = new Organize();
-                    $organizeClass->echoZoneName($type_alias,$zone_name['zone_city'],$zone_name['zone_name'],$zone_name['zone_match_type']);
+                    if($zone_name){
+                        $type_alias = $wpdb->get_var("SELECT zone_type_alias FROM {$wpdb->prefix}zone_type WHERE id={$zone_name['type_id']}");
+                        $organizeClass = new Organize();
+                        $organizeClass->echoZoneName($type_alias,$zone_name['zone_city'],$zone_name['zone_name'],$zone_name['zone_match_type']);
+                    }
                     break;
                 default:
                     break;
