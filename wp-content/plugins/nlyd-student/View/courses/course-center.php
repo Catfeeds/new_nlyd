fbc23fef841a719904cf3be86abfb328c879476f
weihaidong <296656132@qq.com>
2019-01-24 10:21:57 +0800

    1

--- wp-content/plugins/nlyd-student/View/courses/course-center.php
+++ wp-content/plugins/nlyd-student/View/courses/course-center.php
@@ -139,72 +139,8 @@ jQuery(function($) {
                 }
             });
         }
-        pagation('flowMyAdress',1,'')
-
-    
-        // //定义一个空的位置构造函数
-        // function Location(){};
-        // //定义一个可以获得经纬度的方法
-        // Location.prototype.getLocation = function(callback){
-        //     var options = {
-        //     enableHighAccuracy: true,
-        //     maximumAge: 1000
-        //     };
-        //     this.callback = Object.prototype.toString.call(callback) =="[object Function]" ?
-        //         callback :
-        //         function(address){
-        //         // alert(address.province + address.city);
-        //         console.log("getocation(callbackFunction) 可获得定位信息对象");
-        //         };
-        //     var self = this;
-        //     if (navigator.geolocation) {
-        //     //浏览器支持geolocation
-        //     navigator.geolocation.getCurrentPosition(function(position){
-        //         //经度
-        //         var longitude = position.coords.longitude;
-        //         //纬度
-        //         var latitude = position.coords.latitude;
-        //         self.loadMapApi(longitude,latitude);
-        //     }, self.onError, options);
-        //     } else {
-        //     //浏览器不支持geolocation
-        //     alert("浏览器不支持");
-        //     }
-        // };
-        // //定义一个可以解析经纬度的方法，调用百度的api
-        // Location.prototype.loadMapApi = function(longitude, latitude){
-        //     var self = this;
-        //     var oHead = document.getElementsByTagName('HEAD').item(0);
-        //     var oScript= document.createElement("script");
-        //     oScript.type = "text/javascript";
-        //     oScript.src="https://api.map.baidu.com/getscript?v=2.0&ak=A396783ee700cfdb9ba1df281ce36862&services=&t=20140930184510";
-        //     oHead.appendChild(oScript);
-        //     oScript.onload = function(date){
-        //     var point = new BMap.Point(longitude, latitude);
-        //     var gc = new BMap.Geocoder();
-        //     gc.getLocation(point, function(rs) {
-        //         var addComp = rs.addressComponents;
-        //         self.callback(addComp);
-        //     });
-        //     }
-        // };
-        // //定义出现查询位置出现意外的方法
-        // Location.prototype.onError = function(error) {
-        //     switch (error.code) {
-        //     case 1:
-        //         alert("位置服务被拒绝");
-        //         break;
-        //     case 2:
-        //         alert("暂时获取不到位置信息");
-        //         break;
-        //     case 3:
-        //         alert("获取信息超时");
-        //         break;
-        //     case 4:
-        //         alert("未知错误");
-        //         break;
-        //     }
-        // };
+        //初始化
+        pagation('flowMyAdress',1,$('#areaSelect').text())
 
         var area=$.validationLayui.allArea.area;//省市区三级联动
         var posiotionarea=[0,0,0];//初始化位置，高亮展示
@@ -243,80 +179,6 @@ jQuery(function($) {
                 }
             });
         }
-        // alert(userAddress)
-        //调用
-        // if(!$.GetSession('nl_address')){
-            
-        //     if('ontouchstart' in window){//移动端
-        //         var local = new Location();
-        //         local.getLocation(function(res){
-        //             var str=""
-        //             var city=res['city'];
-        //             var province=res['province'];
-        //             var district=res['district'];
-        //             // for(i in res ){
-        //             //     alert(i)
-        //             // str=res[i]+str
-        //             // }
-        //             str=district ? province+'-'+city+'-'+district : province+'-'+city;
-        //             $.SetSession('nl_address',str)
-        //             // alert(str)
-        //             $('#areaSelect').text(str)
-        //             if(str.length>0 && str){
-        //                 var areaValue=str.split('-');
-        //                 $.each(area,function(index,value){
-        //                     if(areaValue[0]==value.value){
-        //                         // posiotionarea=[index,0,0];
-        //                         mobileSelect3.locatePosition(0,index)
-        //                         $.each(value.childs,function(i,v){
-        //                             if(areaValue[1]==v.value){
-        //                                 // posiotionarea=[index,i,0];
-        //                                 mobileSelect3.locatePosition(1,i)
-        //                                 $.each(v.childs,function(j,val){
-        //                                     if(areaValue[2] && areaValue[2]==val.value){
-        //                                         // posiotionarea=[index,i,j];
-        //                                         mobileSelect3.locatePosition(2,j)
-        //                                     }
-        //                                 })
-        //                             }
-        //                         })
-        //                     }
-        //                 })
-        //             }
-        //             $('#flowMyAdress').empty()
-        //             pagation('flowMyAdress',1)
-        //         })
-        //     }else{
-        //         console.log(returnCitySN["cip"]+','+returnCitySN["cname"])
-        //     }
-        // }else{
-        //     var str=$.GetSession('nl_address');
-        //     $('#areaSelect').text(str)
-        //     if(str.length>0 && str){
-        //         var areaValue=str.split('-');
-        //         $.each(area,function(index,value){
-        //             if(areaValue[0]==value.value){
-        //                 // posiotionarea=[index,0,0];
-        //                 mobileSelect3.locatePosition(0,index)
-        //                 $.each(value.childs,function(i,v){
-        //                     if(areaValue[1]==v.value){
-        //                         // posiotionarea=[index,i,0];
-        //                         mobileSelect3.locatePosition(1,i)
-        //                         $.each(v.childs,function(j,val){
-        //                             if(areaValue[2] && areaValue[2]==val.value){
-        //                                 // posiotionarea=[index,i,j];
-        //                                 mobileSelect3.locatePosition(2,j)
-        //                             }
-        //                         })
-        //                     }
-        //                 })
-        //             }
-        //         })
-        //     }
-           
-        //     $('#flowMyAdress').empty()
-        //     pagation('flowMyAdress',1)
-        // }
     });
 })
 </script>
